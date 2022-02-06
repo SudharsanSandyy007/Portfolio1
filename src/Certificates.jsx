92b3c383926a8d9b81d8fece261547dd69d6e791
SudharsanSandyy007 <sudharsandevakumar@gmail.com>
2022-02-07 00:52:01 +0530

    init

---
+++ src/Certificates.jsx
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Certificates.css";
+function Certificates() {
+  return (
+    <div className="certificates" id="certificates">
+      <div className="certificates__top">
+        <h2>Certificates / Achievements</h2>
+      </div>
+      <div className="certificates__bottom">
+        <div className="certificates__bottom__front">
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c1.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c2.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c3.png"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c4.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c1.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c1.jpeg"
+              alt=""
+            />
+          </div>
+          <div>
+            <img
+              src="https://raw.githubusercontent.com/SudharsanSandyy007/Portfolio/main/c1.jpeg"
+              alt=""
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Certificates;
